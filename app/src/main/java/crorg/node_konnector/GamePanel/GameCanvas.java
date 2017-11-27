3e7c49ba5f1971237291e35d1418383e91a3e608
lichal <32421669+lichal@users.noreply.github.com>
2017-11-27 14:03:50 -0500

    Modify and upload Shape, extends drawable

--- app/src/main/java/crorg/node_konnector/GamePanel/GameCanvas.java
+++ app/src/main/java/crorg/node_konnector/GamePanel/GameCanvas.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -16,20 +18,39 @@ import android.view.View;
 public class GameCanvas extends View {
     private Drawable circle;
 
+    private ShapeDrawable mDrawable;
+
     public GameCanvas(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        int x = 10;
+        int y = 10;
+        int width = 100;
+        int height = 100;
+
+        mDrawable = new ShapeDrawable(new OvalShape());
+        // If the color isn't set, the shape uses black as the default.
+        mDrawable.getPaint().setColor(0xff74AC23);
+        // If the bounds aren't set, the shape can't be drawn.
+        mDrawable.setBounds(x, y, x + width, y + height);
     }
 
     protected void onDraw(Canvas canvas){
-        Rect imageBounds = canvas.getClipBounds();
+        mDrawable.draw(canvas);
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event){
         int action = event.getAction();
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+
+
         switch (action){
             case MotionEvent.ACTION_MOVE:
+
+                mDrawable.setBounds(x, y, x +300, y+50);
+                invalidate();
 
         }
         return true;
--- app/src/main/java/crorg/node_konnector/Shapes/Shape.java
+++ app/src/main/java/crorg/node_konnector/Shapes/Shape.java
@@ -1,17 +1,86 @@
 package crorg.node_konnector.Shapes;
 
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Cheng Li on 11/23/2017.
  */
 
-public class Shape {
-    private int connection;
+public class Shape extends Drawable{
+
+    protected int connection;
+    protected int positionX;
+    protected int positionY;
+    protected int height;
+    protected int width;
 
     public Shape(){
 
     }
 
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+
+    }
+
+    @Override
+    public void setAlpha(@IntRange(from = 0, to = 255) int i) {
+
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.OPAQUE;
+    }
+
     public int getConnection(){
         return connection;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setConnection(int connection) {
+        this.connection = connection;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
     }
 }
