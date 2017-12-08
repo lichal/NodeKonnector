13e5c15b17f58e1f028604d47a14a3695607c161
bassorGIT <no-reply>
2017-12-07 20:31:59 -0500

    dd

--- app/src/main/java/crorg/node_konnector/MyFirebaseMessagingService.java
+++ app/src/main/java/crorg/node_konnector/MyFirebaseMessagingService.java
@@ -1,27 +1,56 @@
 package crorg.node_konnector;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import static android.app.Notification.VISIBILITY_PUBLIC;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FCM Service";
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO: Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated.
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        //Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.v("FIREBASE123", "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        notifyUserOfSurpassingFriendScore();
+    }
 
 
+    private void notifyUserOfSurpassingFriendScore() {
+        String CHANNEL_ID = "nodeKonnector_channel_0156";
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.drawable.ic_stat_name);
+        mBuilder.setContentTitle("SECRET MESSAGE");
+        mBuilder.setContentText("You just received a push notification!");
+        mBuilder.setTicker("NODES!");
+        mBuilder.setVibrate(new long[]{0, 100, 100, 100, 100, 100, 100, 1500, 1000, 100, 100, 100, 100, 100, 100, 1500});
+        mBuilder.setColor(0xff00ffff);  // color for app name title in notifications drawer
+        mBuilder.setLights(0xffff00ff, 1000, 500);  // sets flashing lights pattern when phone is locked
+        mBuilder.setAutoCancel(true);
+        mBuilder.setPriority(2);
+        mBuilder.setVisibility(VISIBILITY_PUBLIC);
 
-
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, StartUpScreen.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(StartUpScreen.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        int mNotificationId = 456;
+        mNotificationManager.notify(mNotificationId, mBuilder.build());
     }
 }
 
