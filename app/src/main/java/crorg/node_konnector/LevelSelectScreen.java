16937360db30e995339ad66ea069d7de1e0f77f4
lichal <32421669+lichal@users.noreply.github.com>
2017-12-08 22:41:35 -0500

    push stuff

--- app/src/main/java/crorg/node_konnector/LevelSelectScreen.java
+++ app/src/main/java/crorg/node_konnector/LevelSelectScreen.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import java.io.Serializable;
@@ -18,6 +20,8 @@ public class LevelSelectScreen extends AppCompatActivity implements LevelFragmen
     public static final String HIGHEST_SCORE = "crorg.nodekonnector.HIGHESTSCORE";
     public static final String HIGHEST_LEVEL = "crorg.nodekonnector.HIGHESTLEVEL";
 
+    public static final int RANKING_TRANS = 10023;
+
     private int highestLevel;
     private int highestScore;
 
@@ -29,6 +33,8 @@ public class LevelSelectScreen extends AppCompatActivity implements LevelFragmen
         setContentView(R.layout.activity_main);
 
         LevelContent.ITEMS.clear();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         lock = (ImageView) findViewById(R.id.lock);
 
@@ -57,6 +63,7 @@ public class LevelSelectScreen extends AppCompatActivity implements LevelFragmen
         //Log.v("MESSAGE#45689", "BEFORE sending intent...ItemID: " + item.id);
         startActivity(intent);
         //Log.v("MESSAGE#45689", "AFER sending intent...");
+        finish();
     }
 
 
@@ -64,7 +71,24 @@ public class LevelSelectScreen extends AppCompatActivity implements LevelFragmen
     protected void onPause(){
         super.onPause();
         Log.d("MESSAGE#", "PAUSE");
-        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.rankingMenu) {
+            Intent intent = new Intent(LevelSelectScreen.this,
+                    RankingActivity.class);
+
+            startActivityForResult(intent, RANKING_TRANS );
+            return true;
+        }
+        return false;
     }
 
 }
